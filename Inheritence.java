8bd36cfc608091df82da9aa9d89b58953a891b54
Yashi Srivastava <yashi.18ben1017@abes.ac.in>
2022-05-19 12:48:21 +0530

    Create Inheritence.java

---
+++ Inheritence.java
@@ -0,0 +1,15 @@
+class Vehicle {
+    public int fuelAmount;
+    public int capacity;
+    
+    public void applyBreaks() {
+        
+    }
+}
+
+class Car extends Vehicle {
+    public int fuelAmount;
+    public void noOfWheels() {
+        
+    }
+}
